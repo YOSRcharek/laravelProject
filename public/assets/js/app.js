91a62c9c6a17b67b535b7fc5a26c06a5f76501ff
YOSRcharek <yosryassoura31@gmail.com>
2025-09-17 21:03:54 +0100

    add template

---
+++ public/assets/js/app.js
@@ -0,0 +1,14 @@
+import './bootstrap';
+import './sb-admin-2.min.js';
+import './sb-admin-2.js';
+import './sb-admin-2.js';
+import '../vendor/chart.js/Chart.min.js';
+import '../vendor/chart.js/Chart.js';
+import "./demo/chart-area-demo.js";
+import "./demo/chart-bar-demo.js";
+import "./demo/chart-pie-demo.js";
+import "./demo/datatables-demo.js";
+import '../vendor/jquery/jquery.min.js';
+import '../vendor/bootstrap/js/bootstrap.bundle.min.js';
+import '../vendor/jquery-easing/jquery.easing.min.js';
+
